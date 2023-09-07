99eea1f1b8b3ed72e99cd3287ec16f98893d1a7a
Pawel Maliszewski <pablo999455@gmail.com>
2023-09-07 08:03:09 +0200

    start

---
+++ src/main/java/com/tema26cwicz1/Tema26cwicz1Application.java
@@ -0,0 +1,13 @@
+package com.tema26cwicz1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tema26cwicz1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tema26cwicz1Application.class, args);
+    }
+
+}
